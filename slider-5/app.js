d02e1f65775a8e9bbc5bf6c1f863021109a76970
siddiq-abu <abusiddiq200g3@gmail.com>
2023-09-15 19:03:03 +0000

    swiper-slides

---
+++ slider-5/app.js
@@ -0,0 +1,16 @@
+
+// <!-- Initialize Swiper -->
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+//   alert('Welcome')
+
